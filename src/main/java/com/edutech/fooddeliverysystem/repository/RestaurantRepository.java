3d6cc07cb376c79ee783354afad5da70f9d153fd
helloankitsingh06-bit <helloankitsingh06@gmail.com>
2026-04-14 15:07:10 +0530

    Delete src directory

--- src/main/java/com/edutech/fooddeliverysystem/repository/RestaurantRepository.java
+++
@@ -1,8 +0,0 @@
-package com.edutech.fooddeliverysystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.edutech.fooddeliverysystem.entity.Restaurant;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-}
